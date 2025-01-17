f1a2b730c19bb82feb422207f9ba8e975b923b8b
Govind Dandekar <govind.dandekar@gmail.com>
2025-01-16 16:32:56 -0800

    implement token deletion with useEffect and setTimeout in Root

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,9 +1,30 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { useEffect } from 'react';
+
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
 
 function RootLayout() {
   // const navigation = useNavigation();
+
+  // root component renders first
+  const token = useLoaderData();
+  
+  // provides submit fx to submit Form
+  const submit = useSubmit();
+  
+  // will execute when RootLayout renders, when token changes,
+  // or when submit fx changes (submit won't change)
+  useEffect(() => {
+    if (!token){
+      return;
+    } 
+
+    setTimeout(() => {
+      // no data, target logout action
+      submit(null, {action: '/logout', method: 'post'})
+    }, 1 * 60 * 60 * 1000)
+  }, [token, submit])
 
   return (
     <>
